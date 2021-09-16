dca547ddc5d06d4c00c1340b01584783956c63c9
Eugene <evg.parkhomenko@gmail.com>
2021-09-17 01:05:47 +0300

    выполнил верстку секции experience и skills

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,4 @@
+// Меню
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 const closeMenu = document.querySelector('.menu__close');
@@ -9,3 +10,16 @@ hamburger.addEventListener('click', () => {
 closeMenu.addEventListener('click', () => {
   menu.classList.remove('menu--active');
 });
+
+
+// Метрики
+const metricsValue = document.querySelectorAll('.metrics-list__value');
+const metricsBar = document.querySelectorAll('.metrics-list__bar');
+
+metricsValue.forEach((item, i) => {
+  if (Number(item.innerHTML.slice(0, -1)) >= 100) {
+    metricsBar[i].style.width = '100%';
+  } else {
+    metricsBar[i].style.width = item.innerHTML;
+  }
+})
